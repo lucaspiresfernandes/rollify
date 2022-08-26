3e7bdda1214781c6658ee8c47951d42301bc7875
alyssapiresfernandescefet <alyssapiresfernandes@gmail.com>
2022-08-26 20:07:42 -0300

    Update list.ts

--- src/pages/api/sheet/player/list.ts
+++ src/pages/api/sheet/player/list.ts
@@ -11,15 +11,23 @@ const handler: NextApiHandlerIO<PlayerListApiResponse> = async (req, res) => {
 	if (req.method !== 'GET') return res.status(405).end();
 
 	const player = req.session.player;
+	const npcId = Number(req.query.npcId) || undefined;
 
 	if (!player) return res.json({ status: 'failure', reason: 'unauthorized' });
 
+	const player_id = npcId || player.id;
+
 	const id = req.query.id ? parseInt(req.query.id as string) : undefined;
 
-	const players = await prisma.player.findMany({
-		where: { role: { not: 'ADMIN' }, id: { not: player.id, equals: id } },
-		select: { id: true, name: true },
-	});
+	const players = (
+		await prisma.player.findMany({
+			where: {
+				role: { in: player.admin ? ['PLAYER'] : ['PLAYER', 'NPC'] },
+				id: { not: player_id, equals: id },
+			},
+			select: { id: true, name: true, role: true, showName: true },
+		})
+	).filter((player) => player.showName || player.role === 'PLAYER');
 
 	res.json({ status: 'success', players });
 };
